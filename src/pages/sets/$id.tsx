e54d6e5610b5b7303c0968f03d6a5035e116c14b
bLue <tbdblue@gmail.com>
2019-02-11 01:48:20 +0800

    A: sets page

---
+++ src/pages/sets/$id.tsx
@@ -0,0 +1,127 @@
+import React from 'react';
+import { connect } from 'dva';
+import { ReduxProps, RouteProps } from '@/@types/props';
+import { Row, Col, Card, Table } from 'antd';
+import { getPathParamId } from '@/utils/getPathParams';
+import pages from '@/configs/pages';
+import NotFound from '@/pages/404';
+import PageTitle from '@/components/PageTitle';
+import PageLoading from '@/components/PageLoading';
+import xss from 'xss';
+import { Link } from 'react-router-dom';
+import { numberToAlphabet, urlf } from '@/utils/format';
+import classNames from 'classnames';
+
+interface Props extends ReduxProps, RouteProps {
+  data: ITypeObject<ISet>;
+  session: ISessionStatus;
+  problemResultStats: IUserProblemResultStats;
+}
+
+interface State {
+}
+
+class SetDetail extends React.Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount(): void {
+    window.scrollTo(0, 0);
+  }
+
+  componentWillReceiveProps(nextProps: Readonly<Props>, nextContext: any): void {
+    // 当用户态显现
+    if (!this.props.session.loggedIn && nextProps.session.loggedIn) {
+      nextProps.dispatch({
+        type: 'sets/getDetail',
+        payload: {
+          id: getPathParamId(nextProps.location.pathname, pages.sets.detail),
+          force: true,
+        },
+      })
+    }
+  }
+
+  render() {
+    const {
+      loading, data: allData, session, match, problemResultStats: { acceptedProblemIds, attemptedProblemIds }
+    } = this.props;
+    const id = ~~match.params.id;
+    const data = allData[id] || {} as ISet;
+    if (loading) {
+      return <PageLoading />;
+    }
+    if (!loading && !data.setId) {
+      return <NotFound />;
+    }
+    switch (data.type) {
+      case 'standard':
+        const props: ISetPropsTypeStandard = data.props;
+        return (
+          <PageTitle title={data.title}>
+            <Row gutter={16} className="content-view">
+              <Col xs={24}>
+                <Card bordered={false}>
+                  <h2 className="text-center">{data.title}</h2>
+                  <div dangerouslySetInnerHTML={{ __html: xss(data.description) }}
+                       className="content-area"
+                       style={{ marginTop: '15px' }}
+                  />
+                </Card>
+
+                <Card bordered={false} className="list-card">
+                  {props.sections.map((section, sectionIndex) =>
+                    <div key={sectionIndex}>
+                      <Table dataSource={section.problems}
+                             rowKey={(record: IProblem) => `${record.problemId}`}
+                             loading={loading}
+                             pagination={false}
+                             className="responsive-table"
+                             rowClassName={(record: IProblem) => classNames(
+                               'problem-result-mark-row',
+                               { 'accepted': ~acceptedProblemIds.indexOf(record.problemId) },
+                               { 'attempted': ~attemptedProblemIds.indexOf(record.problemId) }
+                             )}
+                      >
+                        <Table.Column
+                          title={section.title}
+                          key="Title"
+                          render={(text, record: IProblem, problemIndex) => (
+                              <span>
+                                <span>{`${numberToAlphabet(sectionIndex)}${problemIndex + 1}`} - </span>
+                                {record.problemId && record.title ?
+                                  <Link to={urlf(pages.problems.detail, {
+                                    param: { id: record.problemId },
+                                    query: { from: `/sets/${data.setId}` },
+                                  })}>{record.title}</Link> :
+                                  <span>Unknown Problem</span>
+                                }
+                              </span>
+                          )}
+                        />
+                      </Table>
+                    </div>
+                  )}
+                </Card>
+              </Col>
+            </Row>
+          </PageTitle>
+        );
+      default:
+        return <PageTitle title={data.title} />;
+    }
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    loading: !!state.loading.effects['sets/getDetail'],
+    data: state.sets.detail,
+    session: state.session,
+    problemResultStats: state.users.problemResultStats,
+  };
+}
+
+export default connect(mapStateToProps)(SetDetail);
---
+++ src/pages/sets/index.tsx
@@ -0,0 +1,93 @@
+/**
+ * title: Sets
+ */
+
+import React from 'react';
+import { connect } from 'dva';
+import { Table, Pagination, Row, Col, Card } from 'antd';
+import router from 'umi/router';
+import { Link } from 'react-router-dom';
+import limits from '@/configs/limits';
+import pages from '@/configs/pages';
+import { ReduxProps, RouteProps } from '@/@types/props';
+import { urlf } from '@/utils/format';
+import FilterCard from '@/components/FilterCard';
+import ToDetailCard from '@/components/ToDetailCard';
+
+interface Props extends ReduxProps, RouteProps {
+  data: IList<ISet>;
+}
+
+class SetList extends React.Component<Props> {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      window.scrollTo(0, 0);
+    }
+  }
+
+  handlePageChange = page => {
+    router.push({
+      pathname: this.props.location.pathname,
+      query: { ...this.props.location.query, page },
+    });
+  };
+
+  render() {
+    const { loading, data: { page, count, rows } } = this.props;
+    return (
+      <Row gutter={16}>
+        <Col xs={24} md={18} xxl={20}>
+          <Card bordered={false} className="list-card">
+            <Table dataSource={rows}
+                   rowKey="setId"
+                   loading={loading}
+                   pagination={false}
+                   className="responsive-table"
+            >
+              <Table.Column
+                title="Title"
+                key="Title"
+                render={(text, record: ISet) => (
+                  <div>
+                    <Link to={urlf(pages.sets.detail, { param: { id: record.setId } })}>{record.title}</Link>
+                  </div>
+                )}
+              />
+            </Table>
+            <Pagination
+              className="ant-table-pagination"
+              total={count}
+              current={page}
+              pageSize={limits.sets.list}
+              onChange={this.handlePageChange}
+            />
+          </Card>
+        </Col>
+        <Col xs={24} md={6} xxl={4}>
+          <Card bordered={false}>
+            <ToDetailCard label="Go to Set" placeholder="Set ID"
+                          toDetailLink={id => urlf(pages.sets.detail, { param: { id } })} />
+          </Card>
+          <Card bordered={false}>
+            <FilterCard fields={[
+              { displayName: 'Title', fieldName: 'title' },
+            ]} />
+          </Card>
+        </Col>
+      </Row>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    loading: !!state.loading.effects['sets/getList'],
+    data: state.sets.list,
+  };
+}
+
+export default connect(mapStateToProps)(SetList);

a9923022fec532e274aa4644ed94772a1921688d
bLue <tbdblue@gmail.com>
2020-01-09 10:17:50 +0800

    U: index title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,3 @@
-/**
- * title: Home
- */
-
 import React from 'react';
 import { connect } from 'dva';
 import { ReduxProps, RouteProps } from '@/@types/props';
@@ -17,6 +13,7 @@ import classNames from 'classnames';
 import { ContestTypes } from '@/configs/contestTypes';
 import TimeStatusBadge from '@/components/TimeStatusBadge';
 import gStyles from '@/general.less';
+import PageTitle from '@/components/PageTitle';
 
 const TOP_NUM = 5;
 let cachedState = {
@@ -77,113 +74,115 @@ class Index extends React.Component<Props, State> {
     const serverTime = Date.now() - ((window as any)._t_diff || 0);
 
     return <PageAnimation>
-      <Row gutter={16} className="content-view-lg mb-xl">
-        <Col xs={24} className="mt-lg">
-          <h1 className="full-width-inner-content mb-sm">{constants.siteTitle}</h1>
-          <p className="full-width-inner-content text-para" style={{ fontSize: '20px' }}>Practice coding, compete with players, and become a master.</p>
-        </Col>
+      <PageTitle title={null}>
+        <Row gutter={16} className="content-view-lg mb-xl">
+          <Col xs={24} className="mt-lg">
+            <h1 className="full-width-inner-content mb-sm">{constants.siteTitle}</h1>
+            <p className="full-width-inner-content text-para" style={{ fontSize: '20px' }}>Practice coding, compete with players, and become a master.</p>
+          </Col>
 
-        <Col xs={24} className="mt-xl">
-          <h3 className="full-width-inner-content">Recent Contests</h3>
-          <Card bordered={false} className="list-card">
-            <Table
-              dataSource={recentContests.rows}
-              rowKey={(record: IContest) => `${record.contestId}`}
-              loading={recentContestsLoading}
-              pagination={false}
-              className="responsive-table"
-            >
-              <Table.Column
-                title=""
-                key="Type"
-                className="text-right td-icon"
-                render={(text, record: IContest) => (
-                  <span>
-                    {record.type === ContestTypes.Private && <Icon type="lock" />}
-                    {record.type === ContestTypes.Register && <Icon type="team" />}
-                  </span>
-                )}
-              />
-              <Table.Column
-                title="Title"
-                key="Title"
-                render={(text, record: IContest) => (
-                  <Link to={urlf(pages.contests.home, { param: { id: record.contestId } })}>{record.title}</Link>
-                )}
-              />
-              <Table.Column
-                title="Time"
-                key="Time"
-                render={(text, record: any) => (
-                  <Popover content={(
-                    <table>
-                      <tbody>
-                        <tr>
-                          <td className={classNames(gStyles.textRight, gStyles.textBold)}>Start:</td>
-                          <td>{moment(toLongTs(record.startAt)).format('YYYY-MM-DD HH:mm:ss Z')} ({moment(toLongTs(record.startAt)).from(serverTime)})</td>
-                        </tr>
-                        <tr>
-                          <td className={classNames(gStyles.textRight, gStyles.textBold)}>End:</td>
-                          <td>{moment(toLongTs(record.endAt)).format('YYYY-MM-DD HH:mm:ss Z')} ({moment(toLongTs(record.endAt)).from(serverTime)})</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  )}>
-                    <span>{moment(toLongTs(record.startAt)).format('YYYY-MM-DD HH:mm')} ~ {moment(toLongTs(record.endAt)).format('YYYY-MM-DD HH:mm')}</span>
-                  </Popover>
-                )}
-              />
-              <Table.Column
-                title="Status"
-                key="status"
-                render={(text, record: any) => (
-                  <TimeStatusBadge start={toLongTs(record.startAt)} end={toLongTs(record.endAt)} cur={serverTime} />
-                )}
+          <Col xs={24} className="mt-xl">
+            <h3 className="full-width-inner-content">Recent Contests</h3>
+            <Card bordered={false} className="list-card">
+              <Table
+                dataSource={recentContests.rows}
+                rowKey={(record: IContest) => `${record.contestId}`}
+                loading={recentContestsLoading}
+                pagination={false}
+                className="responsive-table"
               >
-              </Table.Column>
-            </Table>
-          </Card>
-        </Col>
-
-        {['month', 'week', 'day'].map((type: 'day' | 'week' | 'month') => {
-          const data = userACRank[type];
-          const rankNameMap = {
-            'day': <span><Icon type="crown" theme="filled" /> DAILY TOP {TOP_NUM}</span>,
-            'week': <span><Icon type="crown" theme="filled" /> WEEKLY TOP {TOP_NUM}</span>,
-            'month': <span><Icon type="crown" theme="filled" /> MONTHLY TOP {TOP_NUM}</span>,
-          };
-          return (
-            <Col xs={24} md={8} key={type} className="mt-xl">
-              <h3 className="full-width-inner-content">{rankNameMap[type]}</h3>
-              <Card bordered={false} className="list-card">
-                <Table
-                  dataSource={data.rows.slice(0, TOP_NUM)}
-                  rowKey={(record: IStatsUserACRankUserStatus) => `${record.user.userId}`}
-                  loading={userACRankloading}
-                  pagination={false}
-                  className="responsive-table"
+                <Table.Column
+                  title=""
+                  key="Type"
+                  className="text-right td-icon"
+                  render={(text, record: IContest) => (
+                    <span>
+                      {record.type === ContestTypes.Private && <Icon type="lock" />}
+                      {record.type === ContestTypes.Register && <Icon type="team" />}
+                    </span>
+                  )}
+                />
+                <Table.Column
+                  title="Title"
+                  key="Title"
+                  render={(text, record: IContest) => (
+                    <Link to={urlf(pages.contests.home, { param: { id: record.contestId } })}>{record.title}</Link>
+                  )}
+                />
+                <Table.Column
+                  title="Time"
+                  key="Time"
+                  render={(text, record: any) => (
+                    <Popover content={(
+                      <table>
+                        <tbody>
+                          <tr>
+                            <td className={classNames(gStyles.textRight, gStyles.textBold)}>Start:</td>
+                            <td>{moment(toLongTs(record.startAt)).format('YYYY-MM-DD HH:mm:ss Z')} ({moment(toLongTs(record.startAt)).from(serverTime)})</td>
+                          </tr>
+                          <tr>
+                            <td className={classNames(gStyles.textRight, gStyles.textBold)}>End:</td>
+                            <td>{moment(toLongTs(record.endAt)).format('YYYY-MM-DD HH:mm:ss Z')} ({moment(toLongTs(record.endAt)).from(serverTime)})</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    )}>
+                      <span>{moment(toLongTs(record.startAt)).format('YYYY-MM-DD HH:mm')} ~ {moment(toLongTs(record.endAt)).format('YYYY-MM-DD HH:mm')}</span>
+                    </Popover>
+                  )}
+                />
+                <Table.Column
+                  title="Status"
+                  key="status"
+                  render={(text, record: any) => (
+                    <TimeStatusBadge start={toLongTs(record.startAt)} end={toLongTs(record.endAt)} cur={serverTime} />
+                  )}
                 >
-                  <Table.Column
-                    title="User"
-                    key="User"
-                    render={(text, record: IStatsUserACRankUserStatus) => (
-                      <UserBar user={record.user} />
-                    )}
-                  />
-                  <Table.Column
-                    title="AC"
-                    key="Accepted"
-                    render={(text, record: IStatsUserACRankUserStatus) => (
-                      <span>{record.accepted}</span>
-                    )}
-                  />
-                </Table>
-              </Card>
-              {data._updatedAt > 0 && <p className="full-width-inner-content text-secondary text-right mt-sm-md" style={{ fontSize: '12px' }}>updated {moment(data._updatedAt).fromNow()}</p>}
-            </Col>
-          );
-        })}
-      </Row>
+                </Table.Column>
+              </Table>
+            </Card>
+          </Col>
+
+          {['month', 'week', 'day'].map((type: 'day' | 'week' | 'month') => {
+            const data = userACRank[type];
+            const rankNameMap = {
+              'day': <span><Icon type="crown" theme="filled" /> DAILY TOP {TOP_NUM}</span>,
+              'week': <span><Icon type="crown" theme="filled" /> WEEKLY TOP {TOP_NUM}</span>,
+              'month': <span><Icon type="crown" theme="filled" /> MONTHLY TOP {TOP_NUM}</span>,
+            };
+            return (
+              <Col xs={24} md={8} key={type} className="mt-xl">
+                <h3 className="full-width-inner-content">{rankNameMap[type]}</h3>
+                <Card bordered={false} className="list-card">
+                  <Table
+                    dataSource={data.rows.slice(0, TOP_NUM)}
+                    rowKey={(record: IStatsUserACRankUserStatus) => `${record.user.userId}`}
+                    loading={userACRankloading}
+                    pagination={false}
+                    className="responsive-table"
+                  >
+                    <Table.Column
+                      title="User"
+                      key="User"
+                      render={(text, record: IStatsUserACRankUserStatus) => (
+                        <UserBar user={record.user} />
+                      )}
+                    />
+                    <Table.Column
+                      title="AC"
+                      key="Accepted"
+                      render={(text, record: IStatsUserACRankUserStatus) => (
+                        <span>{record.accepted}</span>
+                      )}
+                    />
+                  </Table>
+                </Card>
+                {data._updatedAt > 0 && <p className="full-width-inner-content text-secondary text-right mt-sm-md" style={{ fontSize: '12px' }}>updated {moment(data._updatedAt).fromNow()}</p>}
+              </Col>
+            );
+          })}
+        </Row>
+      </PageTitle>
     </PageAnimation>;
   }
 }

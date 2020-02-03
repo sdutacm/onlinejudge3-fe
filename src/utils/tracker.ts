f54f457a4580d624a8f973aea40b9e171de0f2e2
bLue <tbdblue@gmail.com>
2020-02-03 16:20:46 +0800

    U: tracker dev

--- src/utils/tracker.ts
+++ src/utils/tracker.ts
@@ -4,5 +4,21 @@ ReactGA.initialize('UA-91335628-7', {
   titleCase: false,
 });
 
-const tracker = ReactGA;
+let tracker = ReactGA;
+if (process.env.NODE_ENV === 'development') {
+  const genVoidLogger = (type: string) => (...args: any[]) => console.log(`[tracker] [${type}]`, ...args);
+  tracker.initialize = genVoidLogger('initialize');
+  // @ts-ignore
+  tracker.ga = genVoidLogger('ga');
+  tracker.resetCalls = genVoidLogger('resetCalls');
+  tracker.set = genVoidLogger('set');
+  tracker.send = genVoidLogger('send');
+  tracker.pageview = genVoidLogger('pageview');
+  tracker.modalview = genVoidLogger('modalview');
+  tracker.timing = genVoidLogger('timing');
+  tracker.event = genVoidLogger('event');
+  tracker.exception = genVoidLogger('exception');
+  tracker.outboundLink = genVoidLogger('outboundLink');
+}
+
 export default tracker;

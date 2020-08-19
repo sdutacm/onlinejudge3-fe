33c62af4020a1033f42d3229063df02a0428d69c
bLue <tbdblue@gmail.com>
2020-08-20 01:19:10 +0800

    A: request params error prompt

--- src/plugins/onError.ts
+++ src/plugins/onError.ts
@@ -8,15 +8,30 @@ export default {
       const status = e.response.status;
       let msg;
       switch (status) {
-        case 403:
+        case 403: {
           msg = 'You are not allowed to view this page';
           break;
+        }
+        case 422: {
+          const errorData = e.response.data?.data;
+          if (errorData?.msg) {
+            msg = `Invalid request: ${
+              errorData.msg.length < 50 ? errorData.msg : errorData.msg.substr(0, 50) + '...'
+            }`;
+            console.error(
+              `RequestParamsError: [${e.response.config.url}]`,
+              JSON.stringify(errorData, null, 2),
+            );
+          } else {
+            msg = 'Invalid request';
+          }
+          break;
+        }
         default:
           msg = e.message;
       }
       message.error(msg, constants.msgDuration.error);
-    }
-    catch (err) {
+    } catch (err) {
       message.error('Network error', constants.msgDuration.error);
     }
 

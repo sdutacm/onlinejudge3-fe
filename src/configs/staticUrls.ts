926b7779f6a2b9543a5d79b2041308e39b3d0571
bLue <tbdblue@gmail.com>
2020-10-11 10:30:05 +0800

    U: misc static file url

--- src/configs/staticUrls.ts
+++ src/configs/staticUrls.ts
@@ -1,13 +1,13 @@
 const staticUrls = {
   templates: {
     set:
-      'https://acm.sdut.edu.cn/onlinejudge2/misc/%E9%A2%98%E7%9B%AE%E9%9B%86%E5%AF%BC%E5%85%A5%E6%A8%A1%E6%9D%BF.xlsx',
+      'https://acm.sdut.edu.cn/onlinejudge3/assets/misc/%E9%A2%98%E7%9B%AE%E9%9B%86%E5%AF%BC%E5%85%A5%E6%A8%A1%E6%9D%BF.xlsx',
     group:
-      'https://acm.sdut.edu.cn/onlinejudge2/misc/%E7%94%A8%E6%88%B7%E7%BB%84%E5%AF%BC%E5%85%A5%E6%A8%A1%E6%9D%BF.xlsx',
+      'https://acm.sdut.edu.cn/onlinejudge3/assets/misc/%E7%94%A8%E6%88%B7%E7%BB%84%E5%AF%BC%E5%85%A5%E6%A8%A1%E6%9D%BF.xlsx',
     user:
-      'https://acm.sdut.edu.cn/onlinejudge2/misc/onlinejudge%E7%94%A8%E6%88%B7%E5%AF%BC%E5%85%A5%E6%A8%A1%E6%9D%BF.xlsx',
+      'https://acm.sdut.edu.cn/onlinejudge3/assets/misc/onlinejudge%E7%94%A8%E6%88%B7%E5%AF%BC%E5%85%A5%E6%A8%A1%E6%9D%BF.xlsx',
     contestUser:
-      'https://acm.sdut.edu.cn/onlinejudge2/misc/%E6%AF%94%E8%B5%9B%E7%94%A8%E6%88%B7%E5%AF%BC%E5%85%A5%E6%A8%A1%E6%9D%BF.xlsx',
+      'https://acm.sdut.edu.cn/onlinejudge3/assets/misc/%E6%AF%94%E8%B5%9B%E7%94%A8%E6%88%B7%E5%AF%BC%E5%85%A5%E6%A8%A1%E6%9D%BF.xlsx',
   },
 };
 

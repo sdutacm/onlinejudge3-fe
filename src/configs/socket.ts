8e972b0642dd5f281f8cdbc30a94234a9826c206
bLue <tbdblue@gmail.com>
2021-02-10 21:00:11 +0800

    U: socket config

--- src/configs/socket.ts
+++ src/configs/socket.ts
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const socketConfig = {
   judger: {
-    url: isProd ? '/io/judger' : 'http://127.0.0.1:7001/judger',
+    url: isProd ? '/judger' : 'http://127.0.0.1:7001/judger',
   },
 };
 

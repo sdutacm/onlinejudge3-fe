a4f1ee01dc93525d27307ea8fa4eccc211201e4b
bLue <tbdblue@gmail.com>
2024-01-30 11:42:59 +0800

    fix: markdown-it and add ci node_modules cache

--- src/pages/competitions-public/$id/participants.tsx
+++ src/pages/competitions-public/$id/participants.tsx
@@ -16,6 +16,18 @@ import { genshinCharacters } from '@/configs/genshin';
 import markdownit from 'markdown-it';
 
 const md = markdownit().disable('image');
+// Remember the old renderer if overridden, or proxy to the default renderer.
+const defaultRender = md.renderer.rules.link_open || function (tokens, idx, options, env, self) {
+  return self.renderToken(tokens, idx, options);
+};
+
+md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+  // Add a new `target` attribute, or replace the value of the existing one.
+  tokens[idx].attrSet('target', '_blank');
+
+  // Pass the token to the default renderer.
+  return defaultRender(tokens, idx, options, env, self);
+};
 
 export interface Props extends ReduxProps, RouteProps {
   id: number;
@@ -130,7 +142,7 @@ class CompetitionParticipants extends React.Component<Props, State> {
     );
   };
 
-  renderSlogan = (slogan: string) => {
+  renderSlogan = (slogan = '') => {
     const result = md.renderInline(slogan);
     return (
       <p

91c7a608c407c10fd5dd258c8bc1ea70f376ff2b
bLue <tbdblue@gmail.com>
2024-02-03 15:09:29 +0800

    chore: audio screen allow skip

--- src/components/AutoVideoScreen.tsx
+++ src/components/AutoVideoScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactPlayer from 'react-player/file';
+import { Button } from 'antd';
 
 export interface AutoVideoScreenProps {
   url: string;
@@ -9,21 +10,52 @@ export interface AutoVideoScreenProps {
   onError?: (err: any, data?: any) => any;
 }
 
-interface State {}
+interface State {
+  mouseHidden: boolean;
+}
 
 export default class AutoVideoScreen extends React.Component<AutoVideoScreenProps, State> {
   containerRef: HTMLElement | null;
   lastSceneRef: HTMLElement | null;
+  mouseMoveTimer = null;
 
   constructor(props: AutoVideoScreenProps) {
     super(props);
-    this.state = {};
+    this.state = {
+      mouseHidden: true,
+    };
+  }
+
+  handleMouseMove = () => {
+    clearTimeout(this.mouseMoveTimer);
+    this.setState({ mouseHidden: false });
+    this.mouseMoveTimer = setTimeout(() => {
+      this.setState({ mouseHidden: true });
+    }, 2000);
+  };
+
+  skip = () => {
+    this.props.onFinished?.();
+  };
+
+  componentDidMount() {
+    document.addEventListener('mousemove', this.handleMouseMove);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.mouseMoveTimer);
+    document.removeEventListener('mousemove', this.handleMouseMove);
   }
 
   render() {
     return (
       <div
-        style={{ width: '100%', height: '100%', background: this.props.background }}
+        className={this.state.mouseHidden ? 'deep-cursor-none' : ''}
+        style={{
+          width: '100%',
+          height: '100%',
+          background: this.props.background,
+        }}
         ref={(ref) => {
           this.containerRef = ref;
         }}
@@ -37,8 +69,19 @@ export default class AutoVideoScreen extends React.Component<AutoVideoScreenProp
           width="100%"
           height="100%"
         />
-        {/* TODO a skip button */}
-        {/* TODO hide cursor */}
+        <Button
+          type="ghost"
+          onClick={this.skip}
+          style={{
+            color: '#d9d9d9',
+            position: 'fixed',
+            top: '30px',
+            right: '30px',
+            opacity: this.state.mouseHidden ? 0 : 1,
+          }}
+        >
+          跳过
+        </Button>
       </div>
     );
   }
